455e84490855daec3bd1aeea1615a3f3dab62a18
anjumagapu <anjumagapu2628@gmail.com>
2025-01-31 16:04:19 +0530

    fix :updates

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export const metadata = {
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#4A4AF4] flex flex-col items-center justify-center p-4 text-white">
-      <h1 className="text-3xl font-bold mb-8 mt-8">Take your first steps into Web3! 🚀</h1>
-      <p className="text-lg mb-8 text-center max-w-lg">
-      Ready to understand blockchain in 5 minutes? This bite-sized quiz helps you learn Web3 basics
+      <h1 className="text-3xl font-bold mb-8 mt-8 text-center w-full">Take your first steps into Web3! 🚀</h1>
+      <p className="text-lg mb-8 text-center w-full">
+        Ready to understand blockchain in 5 minutes? This bite-sized quiz helps you learn Web3 basics
       </p>
       <div className="w-full max-w-md">
         <Quiz />
